c9b1e4bd03154d8606fd70e1076622c401467f4d
satyamacha <107250700+satyamacha@users.noreply.github.com>
2023-11-16 10:41:17 +0530

    forecast - days edit
    added *1000 to if else in future week forecast for correction in day display

--- weather.js
+++ weather.js
@@ -76,14 +76,14 @@ function showWeatherData(data) {
             currentTempEl.innerHTML =`
             <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather-icon" class="w-icon">
             <div class="other">
-                <div class="day">${window.moment(day.dt).format('ddd')}</div>
+                <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
                 <div class="temp">Night -  ${day.temp.night}&#176; C</div>
                 <div class="temp">Day - ${day.temp.day}&#176; C</div>
             </div>`
         }else{
             otherDayForecast +=
             `<div class="weather-forecast-item">
-                <div class="day">${window.moment(day.dt).format('ddd')}</div>
+                <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
                 <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather-icon" class="w-icon">
                 <div class="temp">Night - ${day.temp.night}&#176; C</div>
                 <div class="temp">Day - ${day.temp.day}&#176; C</div>
